46e665fa4071ec59b9f7c104cf733c8730366229
hasanyesil <hasannyesil97@gmail.com>
2020-06-15 01:37:03 +0300

    Bug fixed. (Users can see only their information now)

--- src/Doctrine/CurrentUserExtension.php
+++ src/Doctrine/CurrentUserExtension.php
@@ -5,11 +5,12 @@ namespace App\Doctrine;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryItemExtensionInterface;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
+use App\Entity\Customer;
 use App\Entity\Order;
 use Doctrine\ORM\QueryBuilder;
 use Symfony\Component\Security\Core\Security;
 
-final class CurrentUserExtension implements QueryCollectionExtensionInterface{
+final class CurrentUserExtension implements QueryCollectionExtensionInterface, QueryItemExtensionInterface{
 
     private $security;
 
@@ -18,21 +19,38 @@ final class CurrentUserExtension implements QueryCollectionExtensionInterface{
         $this->security = $security;
     }
 
+    public function applyToItem(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, array $identifiers, string $operationName = null, array $context = [])
+    {
+        $this->addWhere($queryBuilder,$resourceClass);
+    }
+
     public function applyToCollection(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, string $operationName = null)
     {
         $this->addWhere($queryBuilder,$resourceClass);
     }
 
-    //Where clause for Order entity collection operation. Current users can only see their orders
     private function addWhere(QueryBuilder $queryBuilder, string $resourceClass): void
     {
         $user = $this->security->getUser();
 
-        if (Order::class !== $resourceClass || $user === null){
+        if ($user === null)
             return;
+
+        switch ($resourceClass){
+            case Order::class :
+                //Authenticated users can see only their orders.
+                $rootAlias = $queryBuilder->getRootAliases()[0];
+                $queryBuilder->andWhere(sprintf('%s.customer = :current_user', $rootAlias));
+                $queryBuilder->setParameter('current_user', $user->getId());
+                return;
+                break;
+            case Customer::class :
+                //Authenticated users can see only their account information.
+                $rootAlias = $queryBuilder->getRootAliases()[0];
+                $queryBuilder->andWhere(sprintf('%s.id = :current_user', $rootAlias));
+                $queryBuilder->setParameter('current_user', $user->getId());
+                break;
         }
-        $rootAlias = $queryBuilder->getRootAliases()[0];
-        $queryBuilder->andWhere(sprintf('%s.customer = :current_user', $rootAlias));
-        $queryBuilder->setParameter('current_user', $user->getId());
     }
+
 }
