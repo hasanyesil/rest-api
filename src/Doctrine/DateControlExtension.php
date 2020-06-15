fb88107679bb3f03a9b3e4af17a1c6db5f0a4f87
hasanyesil <hasannyesil97@gmail.com>
2020-06-15 04:34:53 +0300

    Date control extension for preventing expired orders.

--- src/Doctrine/DateControlExtension.php
+++ src/Doctrine/DateControlExtension.php
@@ -1,1 +1,48 @@
 <?php
+namespace App\Doctrine;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryItemExtensionInterface;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
+use App\Entity\Customer;
+use App\Entity\Order;
+use DateTime;
+use Doctrine\ORM\QueryBuilder;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Event\ViewEvent;
+use Symfony\Component\Security\Core\Security;
+
+final class DateControlExtension implements QueryCollectionExtensionInterface, QueryItemExtensionInterface
+{
+
+    private $security;
+    private $operationName;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
+    public function applyToCollection(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, string $operationName = null)
+    {
+        $this->operationName = $operationName;
+        $this->addWhere($queryBuilder,$resourceClass);
+    }
+
+    public function applyToItem(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, array $identifiers, string $operationName = null, array $context = [])
+    {
+        $this->operationName = $operationName;
+        $this->addWhere($queryBuilder,$resourceClass);
+    }
+
+    private function addWhere(QueryBuilder $queryBuilder, string $resourceClass): void
+    {
+        $user = $this->security->getUser();
+
+        if ($user === null || $resourceClass !== Order::class || $this->operationName !== "DELETE")
+            return;
+        $rootAlias = $queryBuilder->getRootAliases()[0];
+        $queryBuilder->andWhere(sprintf('%s.shippingDate > :current_date', $rootAlias));
+        $queryBuilder->setParameter('current_date', (new DateTime('now'))->format('Y-m-d'));
+
+    }
+}
